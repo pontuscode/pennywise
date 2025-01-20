e59a6951edb0d953636e6976fa0ec8a3d4fd0059
Pontus Jarnemyr <pontusjarnemyr@gmail.com>
2025-01-20 18:14:07 +0100

    Rename vue-chrome to chrome

--- chrome/script.js
+++
@@ -1,9 +0,0 @@
-async function fetchData() {
-
-    document.getElementById("date").innerHTML = record.data[0].date;
-    document.getElementById("areaName").innerHTML = record.data[0].areaName;
-    document.getElementById("latestBy").innerHTML = record.data[0].latestBy;
-    document.getElementById("deathNew").innerHTML = record.data[0].deathNew;
-}
-
-fetchData();
