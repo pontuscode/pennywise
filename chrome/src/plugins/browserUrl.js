6c73fe594763e05ccd38eef0fb68ca5db0c46066
Pontus Jarnemyr <pontusjarnemyr@gmail.com>
2025-01-22 22:21:10 +0100

    Detect current base URL

--- chrome/src/plugins/browserUrl.js
+++ chrome/src/plugins/browserUrl.js
@@ -1,15 +1,24 @@
 async function getCurrentTabUrl() {
     return new Promise((resolve, reject) => {
+        const extractBaseUrl = (fullUrl) => {
+            try {
+                const url = new URL(fullUrl);
+                return `${url.protocol}//${url.hostname}`;
+            } catch (error) {
+                console.error('Invalid URL:', fullUrl);
+                return null;
+            }
+        };
         if (typeof browser !== "undefined") { // Check if the "browser" object is available (Firefox)
             browser.tabs.query({ active: true, currentWindow: true })
-                .then(tabs => resolve(tabs[0].url))
+                .then(tabs => resolve(extractBaseUrl(tabs[0].url)))
                 .catch(err => reject(err));
         } else if (typeof chrome !== "undefined") {// Check if the "chrome" object is available (Chrome)
             chrome.tabs.query({ active: true, currentWindow: true }, tabs => {
-                resolve(tabs[0].url);
+                resolve(extractBaseUrl(tabs[0].url));
             });
         } else if (typeof window !== 'undefined' && window.location) { // Check if the "window" object is available (regular web app)
-            resolve(window.location.href);
+            resolve(extractBaseUrl(window.location.href));
         } else {
             reject("No supported browser API found!");
         }
--- chrome/src/views/HomeView.vue
+++ chrome/src/views/HomeView.vue
@@ -2,9 +2,11 @@
 import { ref } from 'vue';
 import { useWebsitesStore } from '@/stores/websites.store';
 import getCurrentTabUrl from '@/plugins/browserUrl';
+import findCouponField from '@/plugins/findCouponField';
 const websitesStore = useWebsitesStore();
 
 const url = ref(null);
+const couponField = ref(null);
 
 async function getUrl() {
   try {
@@ -12,6 +14,18 @@ async function getUrl() {
     console.log(url.value);
   } catch (error) {
     console.error('Error getting URL:', error);
+  }
+}
+
+async function tryFindCouponField() {
+  try {
+    couponField.value = await findCouponField();
+    console.log(couponField.value);
+    if(couponField.value === null) {
+      couponField.value = "Found nothing :(";
+    }
+  } catch (error) {
+    console.error("Error finding coupon field: ", error);
   }
 }
 </script>
@@ -26,14 +40,25 @@ async function getUrl() {
 
     <v-row align-content="center" justify="center">
       <v-col cols="12" md="8" class="text-center">
-        <v-btn @click="getUrl" :style="{ minWidth: 'auto' }" >Get current URL</v-btn>
+        <v-btn @click="getUrl">Get current URL</v-btn>
       </v-col>
     </v-row>
 
     <v-row align-content="center" justify="center">
       <v-col cols="12" md="8" class="text-center">
         <h4 v-if="url">Current url: {{ url }}</h4>
-        <p>Debug: {{ url }}</p> <!-- This line is for debugging -->
+      </v-col>
+    </v-row>
+
+    <v-row align-content="center" justify="center">
+      <v-col cols="12" md="8" class="text-center">
+        <v-btn @click="tryFindCouponField">Find coupon field</v-btn>
+      </v-col>
+    </v-row>
+
+    <v-row align-content="center" justify="center">
+      <v-col cols="12" md="8" class="text-center">
+        <h4 v-if="couponField">Coupon field: {{ couponField.value }}</h4>
       </v-col>
     </v-row>
   </v-container>
